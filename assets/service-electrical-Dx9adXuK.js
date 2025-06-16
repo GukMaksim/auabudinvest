d41bf154b47b23331ecdd58123e96d371be34779
GukMaksim <104374505+GukMaksim@users.noreply.github.com>
2025-06-16 14:06:56 +0000

    Deploying to gh-pages from @ GukMaksim/auabudinvest@bfb58559dddfc4117f1bb6e9072ecd5ed3f40c8d 🚀

--- assets/service-electrical-Dx9adXuK.js
+++
@@ -1,1 +0,0 @@
-const s="/auabudinvest/assets/service-industrial-BIm_DJo_.jpg",a="/auabudinvest/assets/service-plumbing-DIlKQ7wA.jpg",e="/auabudinvest/assets/service-electrical-DlaJv7B7.jpg";export{s as _,a,e as b};
---
+++ assets/team-member_Serhii-EcSnqKqK.js
@@ -0,0 +1,1 @@
+const e="/assets/team-member_Serhii-oY0yha7I.jpg";export{e as _};
